edf527471dd92b894dc0ee56be139d7da913ae61
yasinviv <159425350+yasinviv@users.noreply.github.com>
2024-02-22 11:18:50 +0100

    Initial commit

---
+++ VivesBlog.Ui.Mvc/Controllers/ArticlesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VivesBlog.Ui.Mvc.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
